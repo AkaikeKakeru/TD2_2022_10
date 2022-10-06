10e54cf8e0fa253d9ed43c8b49f4f709cbf7be38
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-06 14:44:42 +0900

    Planeクラスを用意

---
+++ Plane.cpp
@@ -0,0 +1,16 @@
+#include "Plane.h"
+
+void Plane::Initilize(Model* model){
+}
+
+void Plane::Update(){
+}
+
+void Plane::Draw(const ViewProjection viewProjection){
+}
+
+void Plane::Transfer(){
+}
+
+void Plane::Move(){
+}
---
+++ Plane.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include "ViewProjection.h"
+#include "DebugText.h"
+
+#include "MyMatrix.h"
+#include <math.h>
+
+class Plane {
+public: //基本の関数	
+	void Initilize(Model* model);
+	void Update();
+	void Draw(const ViewProjection viewProjection);
+
+public: //固有の関数
+	/// <summary>
+	/// 座標更新
+	/// </summary>
+	void Transfer();
+
+	/// <summary>
+	/// 移動
+	/// </summary>
+	void Move();
+
+private://よく使う変数
+		//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	MyMatrix myMatrix_;
+
+	//デバッグテキスト
+	DebugText* debugText_ = nullptr;
+
+private: //固有の変数
+};
