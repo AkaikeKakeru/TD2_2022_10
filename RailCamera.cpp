f1230d90bd4e8fb51216626c9809fad3ef6b7831
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-08 17:38:03 +0900

    レールカメラの初期化処理を記述

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -1,6 +1,14 @@
 #include "RailCamera.h"
 
-void RailCamera::Initialize(){
+void RailCamera::Initialize(
+	Vector3 position,
+	Vector3 radian){
+	//ワールドトランスフォーム初期設定
+	worldTransform_.translation_ = position;
+	worldTransform_.rotation_ = radian;
+
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
 }
 
 void RailCamera::Update(){
--- RailCamera.h
+++ RailCamera.h
@@ -1,9 +1,19 @@
 #pragma once
+#include "WorldTransform.h"
+#include "ViewProjection.h"
 
 class RailCamera{
 public: //基本メンバ関数
-	void Initialize();
+	/// <param name="worldTransform">ワールドトランスフォーム</param>
+	void Initialize(
+		Vector3 position,
+		Vector3 radian);
 	void Update();
 
 private://よく使うメンバ変数
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
 };
